22b28887118a842284980855440d254d5bc32912
Yicheng <790496165@qq.com>
2018-07-25 16:51:21 +0800

    :art: style: format

--- js/cart.js
+++ js/cart.js
@@ -1,91 +1,91 @@
-Vue.filter("money",function(value,type){
-	return "￥"+value.toFixed(2)+type
+Vue.filter("money", function (value, type) {
+  return "￥" + value.toFixed(2) + type
 })
 new Vue({
-	el: "#app",
-	data: {
-		totalMoney: 0,
-		productList: [],
-		checkAllFlag: false,
-		delFlag: false
-	},
-	filters: {
-		formatMoney: function(value) {
-			return "￥"+value.toFixed(2)//toFixed精度丢失
-		}
-	},
-	mounted: function() {
-		this.$nextTick(function () {
-			this.cartView()
-		})
-	},
-	methods: {
-		cartView: function() {
-			var _this = this
-			this.$http.get("data/cartData.json",{"id": 123}).then(function(res) {
-				_this.productList = res.data.result.list
-			})
-			
-			//es6
+  el: "#app",
+  data: {
+    totalMoney: 0,
+    productList: [],
+    checkAllFlag: false,
+    delFlag: false
+  },
+  filters: {
+    formatMoney: function (value) {
+      return "￥" + value.toFixed(2)//toFixed精度丢失
+    }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.cartView()
+    })
+  },
+  methods: {
+    cartView: function () {
+      var _this = this
+      this.$http.get("data/cartData.json", { "id": 123 }).then(function (res) {
+        _this.productList = res.data.result.list
+      })
+
+      //es6
 			/*this.$http.get("data/cartData.json",{"id": 123}).then(res=>{
 				this.productList = res.data.result.list
 			})*/
-		},
-		changeMoney: function(product, index) {
-			if (index > 0){
-				product.productQuantity++
-			}else {
-				product.productQuantity--
-				if (product.productQuantity < 2){
-					product.productQuantity = 1
-				}
-			}
-			this.calcTotalPrice()
-		},
-		selectedProduct: function(item) {
-			if(typeof item.checked == 'undefined'){
-				//Vue.set(item,"checked",true)//全局注册
-				this.$set(item,"checked",true)//局部注册
-			}else {
-				item.checked = !item.checked
-			}
-			var k=0
-            this.productList.forEach(function(item,index){
-                if(item.checked){ 
-                	k++
-                }
-            })
-            if(k==this.productList.length){
-            	this.checkAllFlag = true
-            }else{
-            	this.checkAllFlag = false
-            }
-			this.calcTotalPrice()
-		},
-		checkAll: function(flag) {
-			this.checkAllFlag = flag
-			var _this = this
-			this.productList.forEach(function(item, index) {
-				if(typeof item.checked == 'undefined'){
-					_this.$set(item,"checked",_this.checkAllFlag)
-				}else {
-					item.checked = _this.checkAllFlag
-				}
-			})
-			this.calcTotalPrice()
-		},
-		calcTotalPrice: function() {
-			var _this =this
-			this.totalMoney = 0
-			this.productList.forEach(function(item, index) {
-				if(item.checked) {
-					_this.totalMoney += item.productPrice*item.productQuantity
-				}
-			})
-		},
-		delProduct: function(){
-			this.productList.splice(this.pindex,1);
-			this.delFlag = false;
-		}
-	}
+    },
+    changeMoney: function (product, index) {
+      if (index > 0) {
+        product.productQuantity++
+      } else {
+        product.productQuantity--
+        if (product.productQuantity < 2) {
+          product.productQuantity = 1
+        }
+      }
+      this.calcTotalPrice()
+    },
+    selectedProduct: function (item) {
+      if (typeof item.checked == 'undefined') {
+        //Vue.set(item,"checked",true)//全局注册
+        this.$set(item, "checked", true)//局部注册
+      } else {
+        item.checked = !item.checked
+      }
+      var k = 0
+      this.productList.forEach(function (item, index) {
+        if (item.checked) {
+          k++
+        }
+      })
+      if (k == this.productList.length) {
+        this.checkAllFlag = true
+      } else {
+        this.checkAllFlag = false
+      }
+      this.calcTotalPrice()
+    },
+    checkAll: function (flag) {
+      this.checkAllFlag = flag
+      var _this = this
+      this.productList.forEach(function (item, index) {
+        if (typeof item.checked == 'undefined') {
+          _this.$set(item, "checked", _this.checkAllFlag)
+        } else {
+          item.checked = _this.checkAllFlag
+        }
+      })
+      this.calcTotalPrice()
+    },
+    calcTotalPrice: function () {
+      var _this = this
+      this.totalMoney = 0
+      this.productList.forEach(function (item, index) {
+        if (item.checked) {
+          _this.totalMoney += item.productPrice * item.productQuantity
+        }
+      })
+    },
+    delProduct: function () {
+      this.productList.splice(this.pindex, 1);
+      this.delFlag = false;
+    }
+  }
 })
